1bdee4a8d585d72071df90879c1b935b5b4c6a13
josh <gleaso22@gmail.com>
2010-12-15 11:40:56 -0800

    should be revized and complete

--- CS474/Assignment5/results/buildgraph.m
+++ CS474/Assignment5/results/buildgraph.m
@@ -6,7 +6,6 @@ function buildgraph(file)
 	set(gca,'XLim', [0.3 8.7]);
 	set(gca,'XTick',1:8);
 	set(gca,'XTickLabel',5:5:40);
-	set(gca,'YLim', [30 70]);
 	legend("Haar    (D-2)","Daubechies-4 ","Daubechies-6 ","Daubechies-8 ","Daubechies-10","Daubechiea-12","Daubechies-14","Daubechies-16","Daubechies-18","Daubechies-20","Daubechies-22");
 	print -dpdf -FCourier:15 output.pdf;
 end
